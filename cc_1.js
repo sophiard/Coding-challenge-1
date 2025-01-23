9196b441a0f9a1c4489c8055ea84be6208e1e815
Sophia Dolan <25srdolan@gmail.com>
2025-01-23 13:24:36 -0500

    Task 1- Employee Information

--- cc_1.js
+++ cc_1.js
@@ -3,6 +3,6 @@ let employeeName = "Sophia Dolan";
 const employeeID = 123;
 var isActive = true;
 
-console.log ("employee Name:", employeeName)
-console.log("Employee ID:", employeeID)
-console.log("Is active:", isActive)
+console.log("Employee Name:", employeeName, "Is a type of:", typeof employeeName);
+console.log("Employee ID:", employeeID, "Is a type of:", typeof employeeID);
+console.log("Is Active:", isActive, "Is a type of:", typeof isActive);
