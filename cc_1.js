1a5d9d2d3d17ed01d74919822a1e83d97d1d2d03
Sophia Dolan <25srdolan@gmail.com>
2025-01-23 12:56:55 -0500

    sdfgn

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+
+let employeeName = "Sophia Dolan"; 
+const employeeID = 123;
+var isActive = true;
+
+console.log ("employee Name:", employeeName)
+console.log("Employee ID:", employeeID)
+console.log("Is active:", isActive)
