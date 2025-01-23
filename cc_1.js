fac6d32a5e29a84137c1381db5784f675cee3c63
Sophia Dolan <25srdolan@gmail.com>
2025-01-23 13:25:16 -0500

    Task 2- Product detail

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,11 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "Is a type of:", typeof employeeName);
 console.log("Employee ID:", employeeID, "Is a type of:", typeof employeeID);
 console.log("Is Active:", isActive, "Is a type of:", typeof isActive);
+
+let productName = "fishgold";
+const productPrice = 1.99;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "Is a type of", typeof productName);
+console.log("Product Price:", productPrice, "Is a type of", typeof productPrice);
+console.log("Is Available:", isAvailable, "Is a type of", typeof isAvailable);
